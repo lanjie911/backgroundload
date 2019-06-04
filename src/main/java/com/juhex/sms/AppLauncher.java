4e28773338ab089e4160e36b301ec7ca30d5c9e9
lanjie911 <lanjie911@hotmail.com>
2019-06-04 13:31:50 +0800

    提交静态文件

--- src/main/java/com/juhex/sms/AppLauncher.java
+++ src/main/java/com/juhex/sms/AppLauncher.java
@@ -1,3 +1,11 @@
+/**
+ * springboot的静态资源，默认路径按照优先级顺序是</p>
+ * /META-INF/resources/
+ * /resources/
+ * /static/
+ * /public/
+ * 默认这些文件夹都建立在工程的resources原文件夹下即可
+ */
 package com.juhex.sms;
 
 import org.springframework.boot.SpringApplication;
